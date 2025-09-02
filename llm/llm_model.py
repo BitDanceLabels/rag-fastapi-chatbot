42be852821fe46c28e3a2aeefd9980f1725886b1
Hao Nguyen <haonguyen@192.168.2.3>
2025-09-03 01:53:37 +0700

    done demo

--- llm/llm_model.py
+++ llm/llm_model.py
@@ -2,15 +2,19 @@ from langchain_ollama import ChatOllama
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
+from pathlib import Path
 
 # --- Load vector store và retriever ---
-path_file = "../embedding/chroma_langchain_db"
+BASE_DIR = Path(__file__).resolve().parent.parent
+DB_PATH = BASE_DIR / "embedding" / "chroma_langchain_db"
+
+
 embedding = HuggingFaceEmbeddings(
     model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
 )
 
 vector_store = Chroma(
-    persist_directory=path_file,
+    persist_directory=str(DB_PATH),
     embedding_function=embedding,
     collection_name="customer-service",
 )
@@ -31,7 +35,7 @@ prompt = ChatPromptTemplate.from_messages(
     [
         (
             "system",
-            "Bạn là trợ lý ảo tên là Quatest 3 Chatbot. "
+            "Bạn là trợ lý ảo. "
             "Nhiệm vụ tóm tắt thông tin và trả lời cho khách hàng.",
         ),
         (
@@ -42,23 +46,51 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-# --- Truy vấn ---
-question = "Giới thiệu cho tôi về trung tâm kỹ thuật 3"
-docs = retriever.invoke(question)
-# for doc in docs:
-#     print(f"{doc.id}: {doc.page_content}\n")
-
-# Lấy nội dung từ các chunk
-contents = [doc.page_content for doc in docs]
-context = "\n".join(contents)
-
-# --- Kết nối LLM và prompt ---
+# # --- Truy vấn ---
+# question = "Giới thiệu về Quatest 3"
+# docs = retriever.invoke(question)
+# # for doc in docs:
+# #     print(f"{doc.id}: {doc.page_content}\n")
+#
+# # Lấy nội dung từ các chunk
+# contents = [doc.page_content for doc in docs]
+# context = "\n".join(contents)
+#
+## --- Kết nối LLM và prompt ---
 chain = prompt | llm
-ai_msg = chain.invoke(
-    {
-        "question": question,
-        "context": context,
-    }
-)
+# ai_msg = chain.invoke(
+#     {
+#         "question": question,
+#         "context": context,
+#     }
+# )
 
-print(ai_msg.content)
+# for chunk in chain.stream(
+#     {
+#         "question": question,
+#         "context": context,
+#     }
+# ):
+#     print(chunk.content, end="", flush=True)
+
+while True:
+    user_input = input("You: ")
+    if user_input.lower() in ["exit", "quit", "bye"]:
+        print("\nAI: bye bye")
+        break
+
+    print("AI: ", end="", flush=True)
+    question = user_input.strip()
+
+    docs = retriever.invoke(question)
+    contents = [doc.page_content for doc in docs]
+    context = "\n".join(contents)
+
+    for chunk in chain.stream(
+        {
+            "question": question,
+            "context": context,
+        }
+    ):
+        print(chunk.content, end="", flush=True)
+    print("\n")
