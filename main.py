807febc3c5e6303b5cde80f6ec1e00e9fa7e6b16
haontuhcmut <nguyenminhhao1188@gmail.com>
2025-09-15 12:18:56 +0700

    nothing

---
+++ main.py
@@ -0,0 +1,11 @@
+from collection.pdf_collection import PDFCollection
+import asyncio
+
+path_file = "./document/data.pdf"
+
+loader = PDFCollection(path_file)
+
+docs = asyncio.run(loader.loader_pdf())
+
+print(docs)
+
